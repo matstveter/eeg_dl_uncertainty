9c1bc75de4597a660e2a1171bf4ab2eb1b983852
matstveter <mats.tveter@gmail.com>
2024-03-26 12:14:17 +0100

    New structure for experiments

---
+++ src/eegDlUncertainty/experiments/SingleModelExperiment.py
@@ -0,0 +1,20 @@
+from eegDlUncertainty.experiments.mainExperiment import BaseExperiment
+from eegDlUncertainty.models.classifiers.main_classifier import MainClassifier
+
+
+class SingleModelExperiment(BaseExperiment):
+    def create_model(self, **kwargs):
+
+        hyperparameters = {"in_channels": self.dataset.num_channels,
+                           "num_classes": self.dataset.num_classes,
+                           "time_steps": self.dataset.eeg_len,
+                           "save_path": self.paths,
+                           "lr": self.learning_rate}
+        kwargs.update(hyperparameters)
+        self.model = MainClassifier(model_name=self.model_name, **kwargs)
+
+    @staticmethod
+    def add_hyperparameters(change_in_hyperparameters, **kwargs):
+        for k, v in change_in_hyperparameters.items():
+            kwargs[k] = v
+        return kwargs
